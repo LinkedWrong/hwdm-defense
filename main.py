0a5cb825223abb7f0c8151483214643b61318fbe
LinkedWrong <79053675+LinkedWrong@users.noreply.github.com>
2024-11-25 19:47:07 +0300

    Update

--- main.py
+++ main.py
@@ -7,22 +7,42 @@ U = list(product(M, M))
 def transitive_condition(relation, a, b, c):
     return (((a, b) in relation) and ((b, c) in relation)) <= ((a, c) in relation)
 
-def is_transitive(relation, n):
+def reflexive_condition(relation, a):
+    return (a, a) in relation
+
+def symmetric_condition(relation, a, b):
+    return ((a, b) in relation) <= ((b, a) in relation)
+
+
+def is_transitive(relation):
     for a, b, c in product(M, repeat=3):
         if not transitive_condition(relation, a, b, c):
             return False
     return True
 
-idx = 1
+def is_reflexive(relation):
+    for a in M:
+        if not reflexive_condition(relation, a):
+            return False
+    return True
 
-print("relation", idx, ":" , "empty")
+def is_symmetric(relation):
+    for a, b in product(M, repeat=2):
+        if not symmetric_condition(relation, a, b):
+            return False
+    return True
+
+def is_equivalent(relation):
+    return is_reflexive(relation) and is_symmetric(relation) and is_transitive(relation)
+
+idx = 0
 for mask in range(1, 2 ** len(U)):
     s = set()
     for i in range(len(U)):
         if mask & (1 << i):
             s.add(U[i])
 
-    if is_transitive(s, n):
+    if is_equivalent(s):
         idx += 1
         print("relation", idx, ":" , *s)
 
